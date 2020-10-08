78b94467a70987277b737ff86331adc21abdeede
angelask <angela.strayhorn@yahoo.com>
2020-10-07 21:41:15 -0500

    Updates

---
+++ precache-manifest.ee990e1f8db3a96bdc501dd992c7c372.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23a920e99aa2f8ba31a2f208de152536",
+    "url": "/angela_react_portfolio_ask/index.html"
+  },
+  {
+    "revision": "3ffb4267f6ec94236db4",
+    "url": "/angela_react_portfolio_ask/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a7691e834d58280de617",
+    "url": "/angela_react_portfolio_ask/static/css/main.ac0ce8fe.chunk.css"
+  },
+  {
+    "revision": "3ffb4267f6ec94236db4",
+    "url": "/angela_react_portfolio_ask/static/js/2.1495017c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/angela_react_portfolio_ask/static/js/2.1495017c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7691e834d58280de617",
+    "url": "/angela_react_portfolio_ask/static/js/main.8c22c85e.chunk.js"
+  },
+  {
+    "revision": "918362c92270486b96d5",
+    "url": "/angela_react_portfolio_ask/static/js/runtime-main.ad2d1e4c.js"
+  },
+  {
+    "revision": "fa99aff711662c34d9acf4c01364c1ec",
+    "url": "/angela_react_portfolio_ask/static/media/angela.fa99aff7.jpg"
+  }
+]);
