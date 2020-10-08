889bc62cc5561f860cfc4ba4418ace16e94d777b
angelask <angela.strayhorn@yahoo.com>
2020-10-08 11:36:59 -0500

    trying to fix deploy issues

--- src/components/Navigation/NavBar.js
+++ src/components/Navigation/NavBar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
+import {Link} from 'react-router-dom'
 
 class NavBar extends Component {
   render(){
@@ -8,9 +9,9 @@ class NavBar extends Component {
         
         <Navbar bg="light" variant="light" expand="lg" sticky="top">
           <Navbar.Brand
-            href="/"
+           
           >
-            Angela Keys Engineering Home Page
+           <Link to="/angela_react_portfolio_ask" >Angela Keys Engineering Home Page </Link>
           </Navbar.Brand>
           <Navbar.Toggle
             aria-controls="basic-navbar-nav"
@@ -18,14 +19,14 @@ class NavBar extends Component {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
               <Nav.Link
-                href="/portfolio"
-              >
-                Portfolio
+                
+              ><Link to="/angela_react_portfolio_ask/portfolio" >
+                Portfolio</Link>
               </Nav.Link>
               <Nav.Link
-                href="/contact"
-              >
-                Contact Us
+               
+              ><Link to="/angela_react_portfolio_ask/contact" >
+                Contact Us</Link>
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
